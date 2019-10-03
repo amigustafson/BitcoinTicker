15ab0048a55dc166b1784e0e541910d455a59870
amigustafson <ami.gustafson@hotmail.com>
2019-10-03 13:15:34 +0200

    add express, body-parser, requset

---
+++ script.js
@@ -0,0 +1,9 @@
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
