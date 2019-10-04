3b4d5786e8dce784d86aa725611699fb8970c4da
amigustafson <ami.gustafson@hotmail.com>
2019-10-04 12:26:04 +0200

    fix express, body-parser, request after git reset

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ const request = require("request");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/", function(req, res){
+  console.log(req.body.fiat);
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
