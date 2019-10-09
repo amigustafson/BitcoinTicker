b8f5db267917a2753524be41a1aed278ec8c6ec4
amigustafson <ami.gustafson@hotmail.com>
2019-10-09 15:35:42 +0200

    add return message

--- script.js
+++ script.js
@@ -14,8 +14,17 @@ app.get("/", function(req, res){
 
 
 app.post("/", function(req, res){
-  console.log(req.body.fiat);
+  //console.log(req.body.fiat);
+
+  request("https://apiv2.bitcoinaverage.com/indices/crypto/ticker/BTCUSDC", function(error, response, body){
+      var data = JSON.parse(body);
+      var lastPrice = data.last;
+
+      res.send("<h1>The current price of BitCoin is </h1>" + lastPrice);
+  });
 });
+
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
